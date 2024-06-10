fce953321631ae29ceb24d450bf3cfa38ab708bd
Armin Raad <armin3600z@gmail.com>
2024-06-10 18:56:29 +0100

    mod working

---
+++ data.lua
@@ -0,0 +1,6 @@
+data.raw["container"]["kr-medium-container"].inventory_size = 160
+data.raw["logistic-container"]["kr-medium-active-provider-container"].inventory_size = 160
+data.raw["logistic-container"]["kr-medium-buffer-container"].inventory_size = 160
+data.raw["logistic-container"]["kr-medium-passive-provider-container"].inventory_size = 160
+data.raw["logistic-container"]["kr-medium-requester-container"].inventory_size = 160
+data.raw["logistic-container"]["kr-medium-storage-container"].inventory_size = 160
